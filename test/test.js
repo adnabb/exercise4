53a5d453e0b57e34543f340fe82939d15c6e1e9e
chenshiyu <chenshiyu@ygsoft.com>
2019-07-31 16:03:59 +0800

    feat/exercise4

--- test/test.js
+++ test/test.js
@@ -4,19 +4,25 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
           done()
         }, 0)
       }
     }
+    /**
+     * 这里的say函数体是由obj调用的，this指向obj
+     */
     obj.say()
   }) 
 
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      this.should.equal(global)
     }
+    /**
+     * 这里的test函数体的this指向global
+     */
     test()
   })
 
@@ -26,7 +32,7 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +45,7 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
